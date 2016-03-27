0327ed608528e09ac52dc1fdaab23b48ad92686a
Brian <brianyoungtw@gmail.com>
2016-03-27 13:24:33 +0800

    Final version of resume

--- js/helper.js
+++ js/helper.js
@@ -13,20 +13,20 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span>%data%</span><hr/>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="yellow-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="yellow-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="yellow-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="yellow-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="yellow-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="yellow-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="yellow-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -40,7 +40,7 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" id="proj-pic">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -58,6 +58,8 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
+var HTMLaudio = '<audio controls id="audio"><source src="./music/ode_to_joy.mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>';
+
 
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
@@ -68,6 +70,7 @@ $(document).ready(function() {
     $('#name').html(iName);  
   });
 });
+
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
@@ -85,7 +88,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x =loc.pageX;
+  var y =loc.pageY;
+  logClicks(x,y);
 });
 
 
@@ -109,10 +114,8 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
+  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
+  // <div id="map">, which is appended as part of an exercise late in the course.
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
@@ -129,20 +132,16 @@ function initializeMap() {
     locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
+    // the locations array
+    for (var school in education.schools) {
+      locations.push(education.schools[school].location);
+    }
 
     // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
+    // the locations array
+    for (var job in work.jobs) {
+      locations.push(work.jobs[job].location);
+    }
 
     return locations;
   }
@@ -209,16 +208,17 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    for (var place in locations) {
+
       // the search request object
       var request = {
-        query: place
+        query: locations[place]
       };
 
       // Actually searches the Google Maps API for location data and runs the callback
       // function with the search results after each search.
       service.textSearch(request, callback);
-    });
+    }
   }
 
   // Sets the boundaries of the map based on pin locations
@@ -238,11 +238,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,217 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// Name
+	function displayName() {
+		var formattedName = HTMLheaderName.replace("%data%","Brian Young");
+		var formattedRole = HTMLheaderRole.replace("%data%","Rule breaker");
+
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+	};
+	displayName();
+
+// bio
+	var bio = {
+		"name": "Brian Young",
+		"role": "Rule breaker",
+		"contacts": {
+			"mobile": HTMLmobile.replace("%data%","控巴控控"),
+			"email": HTMLemail.replace("%data%","wrongemail@gmail.com"),
+			"github": HTMLgithub.replace("%data%","brianyoungtw"),
+			"twitter": HTMLtwitter.replace("%data%","brianyoungtw"),
+			"blog": HTMLblog.replace("%data%","brianyoungtw.blogspot.tw/"),
+			"location": HTMLlocation.replace("%data%","Taipei"),
+		},
+		"welcomeMessage": HTMLwelcomeMsg.replace("%data%","Hi from inside your screen!"),
+		"skills": ["Being bad-ass","Breathing","Having fun","Staring at you from inside your screen"],
+		"bioPic": HTMLbioPic.replace("%data%","images/Brian_pic.jpg"),
+		//display
+		"display": function(){
+			$("#topContacts").append(bio.contacts.mobile);
+			$("#topContacts").append(bio.contacts.email);
+			$("#topContacts").append(bio.contacts.twitter);
+			$("#topContacts").append(bio.contacts.github);
+			$("#topContacts").append(bio.contacts.blog);
+			$("#topContacts").append(bio.contacts.location);
+			$("#header").append(bio.bioPic);
+			$("#header").append(bio.welcomeMessage);
+
+			if (bio.skills.length >0) {
+				$("#header").append(HTMLskillsStart);
+				var formattedSkills = HTMLskills.replace("%data%",bio.skills[0]);
+				$("#skills").append(formattedSkills);
+				var formattedSkills = HTMLskills.replace("%data%",bio.skills[1]);
+				$("#skills").append(formattedSkills);
+				var formattedSkills = HTMLskills.replace("%data%",bio.skills[2]);
+				$("#skills").append(formattedSkills);
+				var formattedSkills = HTMLskills.replace("%data%",bio.skills[3]);
+				$("#skills").append(formattedSkills);
+			};
+		}
+	};
+	bio.display();
+
+// Work
+	var work = {
+		"jobs": [
+			{
+			"employer": "The Future of Learning",
+			"url": "https://www.facebook.com/groups/EdTech.NewEdWays/",
+			"title": "掃地僧",
+			"dates": "Mar.2015~current",
+			"location": "Taipei",
+			"description": "The first edtech community in Taiwan - Let's make learning exciting!"
+			},
+			{
+			"employer": "EdSurge",
+			"url": "https://www.linkedin.com/in/brian-young-12b64331",
+			"title": "Analyst / Sales Associate intern",
+			"dates": "Aug.2015~Dec.2015",
+			"location": "San Francisco",
+			"description": "dummy text"
+			},
+			{
+			"employer": "IBM Taiwan",
+			"url": "https://www.linkedin.com/in/brian-young-12b64331",
+			"title": "Sales Representative / Consultant",
+			"dates": "Apr.2013~Feb.2015",
+			"location": "Taipei / Hsinchu",
+			"description": "dummy text"
+			}
+		],
+		"display": function(){
+			for (job in work.jobs) {
+				$("#workExperience").append(HTMLworkStart);
+				var fomattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer).replace("#",work.jobs[job].url);
+				var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+				var formattedEmployerTitle = fomattedEmployer + formattedTitle;
+				$(".work-entry:last").append(formattedEmployerTitle);
+
+				var formattedDate = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+				$(".work-entry:last").append(formattedDate);
+
+				var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+				$(".work-entry:last").append(formattedLocation);
+
+				var formattedDescription = HTMLprojectDescription.replace("%data%",work.jobs[job].description);
+				$(".work-entry:last").append(formattedDescription);
+			};
+		}
+	}
+	work.display();
+
+// projects
+	var projects = {
+		"projects": [
+			{
+				"name": "The Future of Learning",
+				"dates": "Mar.2015-Current",
+				"description": "Come join in the conversation at the first edtech community in Taiwan!",
+				"image": "./images/web_dev.jpg",
+				"url": "https://www.facebook.com/groups/EdTech.NewEdWays/",
+				"other":""
+			},
+			{
+				"name": "Hayao Miyazaki(宮崎駿) fan project",
+				"dates": "TBD",
+				"description": "Love his animations and music!",
+				"image": "./images/dodoro.jpeg",
+				"url": "",
+				"other":""
+			},
+			{
+				"name": "The happy project",
+				"dates": "Mar.2016",
+				"description": "Please play the song below - 'Ode to joy' while singing along using these lyrics: <br></br><p>All my clients drive me crazy</p><p>never show no guts at all</p><p>For the peanuts that they pay me</p><p>they get logos 10 feet tall</p><p>Want to see three new directions</p><p>for tomorrow’s drop dead line</p><p>Picks the worst and mixes sections </p><p>we wind up with Frankenstein.<br></br></p>",
+				"image": "",
+				"url": "http://www.brianyoungtw.com",
+				"other": ""
+			}
+		],
+		"display": function() {
+			for(project in projects.projects) {
+			$("#projects").append(HTMLprojectStart);
+			$(".project-entry:last").append(HTMLprojectTitle.replace("%data%",projects.projects[project].name).replace("#",projects.projects[project].url));
+			$(".project-entry:last").append(HTMLprojectDates.replace("%data%",projects.projects[project].dates));
+			$(".project-entry:last").append(HTMLprojectDescription.replace("%data%",projects.projects[project].description));
+			$(".project-entry:last").append(HTMLprojectImage.replace("%data%",projects.projects[project].image));
+			$(".project-entry:last").append(projects.projects[project].other);
+			}
+			$(".project-entry:last").append(HTMLaudio);
+		}
+	};
+	projects.display();
+
+// education
+	var education = {
+		"schools": [
+		{
+			"name": "NTHU",
+			"location": "Hsinchu, Taiwan",
+			"degree": "B.S.",
+			"majors":["Quantitative Finance"],
+			"dates": "2008 - 2011",
+			"url": "http://www.qf.nthu.edu.tw/bin/home.php?Lang=en"
+		},
+		{
+			"name": "Universitaet Tuebingen",
+			"location": "Tuebingen, Germany",
+			"degree": "Exchange student",
+			"majors":["Economics", "BA"],
+			"dates": "2010",
+			"url": "https://www.uni-tuebingen.de/"
+		},
+		{
+			"name": "Zhejiang University",
+			"location": "Zhejiang, China",
+			"degree": "Exchange student",
+			"majors":["Economics"],
+			"dates":"2009",
+			"url": "http://www.zju.edu.cn/english/"
+		},
+		],
+		"onlineCourses": [
+		{
+			"title": "Front End Web Dev. Nanodegree",
+			"school": "Udacity",
+			"dates": "Feb. 2016 - Current",
+			"schoolUrl": "https://www.udacity.com/",
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		}
+		],
+		//display
+		"display": function(){
+			for(edu in education.schools) {
+				$("#education").append(HTMLschoolStart);
+				$(".education-entry:last").append(HTMLschoolName.replace("%data%",education.schools[edu].name).replace("#",education.schools[edu].url));
+				$(".education-entry:last").append(HTMLschoolDegree.replace("%data%",education.schools[edu].degree));
+				$(".education-entry:last").append(HTMLschoolDates.replace("%data%",education.schools[edu].dates));
+				$(".education-entry:last").append(HTMLschoolLocation.replace("%data%",education.schools[edu].location));
+				for(major in education.schools[edu].majors){
+					$(".education-entry:last").append(HTMLschoolMajor.replace("%data%",education.schools[edu].majors[major]));
+				};
+			};
+			//onlineCourses
+			$("#education").append(HTMLonlineClasses);
+			for(edu in education.onlineCourses) {
+				$("#education").append(HTMLschoolStart);
+				$(".education-entry:last").append(HTMLonlineTitle.replace("%data%",education.onlineCourses[edu].title).replace("#",education.onlineCourses[edu].schoolUrl));
+				$(".education-entry:last").append(HTMLonlineSchool.replace("%data%",education.onlineCourses[edu].school));
+				$(".education-entry:last").append(HTMLonlineDates.replace("%data%",education.onlineCourses[edu].dates));
+				$(".education-entry:last").append(HTMLonlineURL.replace("%data%",education.onlineCourses[edu].url));
+			};
+		}
+	};
+	education.display();
+
+// Map
+	$("#mapDiv").append(googleMap);
+
+// Footer Contacts
+	var footer = {
+		"display": function(){
+			$("#footerContacts").append(bio.contacts.mobile);
+			$("#footerContacts").append(bio.contacts.email);
+			$("#footerContacts").append(bio.contacts.twitter);
+			$("#footerContacts").append(bio.contacts.github);
+		}
+	};
+	footer.display();
